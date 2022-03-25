12c1ccbda1e6812f52bdc3af1059658e532e7162
Monica <77224723+Energix11@users.noreply.github.com>
2022-03-25 15:56:06 -0700

    Removed unnecessary code from summary file

--- src/SummaryTab.py
+++ src/SummaryTab.py
@@ -60,38 +60,5 @@ intro = (
             )
             ])
 )
-# html.DIV(
-#     children='',
-#     style={
-#             'textAlign': 'center',
-#             'color': colors['text']
-#         }
-# )
 
 
-# written <- div(
-#     h1(
-# 	'Welcome',
-# 	style = list(
-#         'color' = 'var(--bs-primary)',
-#         'textAlign' = 'center'
-#         )
-#         )
-# ),
-#      h3(
-#          'Surgical systems are a fundamental part of an effective health care system.'
-#          ),
-#      h4(
-#          'Data related to surgical system functioning is collected, synthesized and published in the BC Data Catalogue and displayed here by total values across time and by hospital and procedure from 2009 to the most recent fiscal quarter.  There are two main types of information contained within two tabs.'
-#          ),
-#      h4(
-#          'Data for the counts of waiting and completed surgical cases is presented in the first tab to the left.  This data represents the counts of the number of patients waiting for surgery and the counts of the number of surgeries completed within each fiscal quarter. This tab is important for understanding the demands and productivity of the surgical system'
-#          ),
-#      h4(
-#          'Data for the average wait times for completed surgical cases is presented in the second tab to the left.  This data represent the time, in weeks, patients had to wait before their surgery was completed.  For all the surgeries completed within each quarter, the time 50% of patients had to wait is represented by the 50th percentile data and the number of weeks 90% of patients had to wait is represented by the 90th percentile data.  This tab is important for understanding how long patients have to wait before they can get surgery.'
-#          ),
-#      h4(
-#          'Individual health authoritise can be selected from the dropdown menu.'
-#      )
-
-# )
